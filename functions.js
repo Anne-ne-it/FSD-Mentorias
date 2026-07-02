47d3d7ae5fb75e058eb0eb577c7244339d10c8ee
Ane Nemo <anne.aikau@gmail.com>
2026-07-02 16:22:42 +0200

    rama feature/functions y primeras funciones

---
+++ functions.js
@@ -0,0 +1,10 @@
+function sum (a, b) {
+    return a + b
+}
+
+function saludar (saludar, nombre) {
+    return `${saludar}, ${nombre}`
+}
+
+console.log(sum(4, 7))
+console.log(saludar("hola", "ane"))
